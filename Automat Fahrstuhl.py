41b69aaa069500dce3e83a8ec510ec774aa67a57
JustWatcher124 <jw124@jw124.fritz.box>
2020-09-06 00:41:30 +0200

    Built in swich, function based

--- Automat Fahrstuhl.py
+++ Automat Fahrstuhl.py
@@ -1,3 +1,6 @@
+import time
+
+
 def hochfahren(anzahl):
     print('Lift fährt', anzahl, 'Stockwerk(e) nach oben')
 
@@ -6,36 +9,46 @@ def herunterfahren(anzahl):
     print('Lift fährt', anzahl, 'Stockwerk(e) nach unten')
 
 
-def ZustandWechseln(eingabe, zustand):
-    '''Funktion, die das Wechseln des Stockwerks simuliert
-    Parameter: eingabe = String
-    Rückgabewert: zustand = String'''
-    switcherZustand = {
-    "Lift im EG": eingabefunk(eingabe, "EG"),
-    "Lift im UG": eingabefunk(eingabe, "UG"),
-    "Lift im OG": eingabefunk(eingabe, "OG")
-    }
-    return switcherZustand.get(zustand, "Bitte erneut Taste drücken")
+def stockwerkdistanz(wohin, wojetzt, switcher):
+    # print(switcher.get(wojetzt)-switcher.get(wohin))
+    return switcher.get(wohin)-switcher.get(wojetzt)
 
 
-def eingabefunk(eingabe, zustandstockwerk):
-    wvstockwerke = stockwerksanzahl(eingabe, zustandstockwerk)
-    print("Der Lift fährt", wvstockwerke)
-    switcher={
-    "UG": "Lift im UG" ,
-    "EG": "Lift im EG",
-    "OG": "Lift im OG"
+print("Willkommen im Aufzug der ZUK-unft AG\n"
+"Sie befinden sich im Untergeschoss,\n"
+"In diesem Gebäude befinden sich das (U)ntergeschoss, (E)rdgeschoss und das (O)bergeschoss\n"
+"Der Aufzug fährt nur mit theorethischer Geschwindigkeit, braucht also für jede Strecke nur 1 Sekunde")
+woistaufzug = "U"
+unnecessary = 0
+switchlist={
+    0 : "U",
+    1 : "E",
+    2 : "O",
+    "U": 0,
+    "E": 1,
+    "O": 2
     }
-    return switcher.get(eingabe)
-
-def stockwerksanzahl(eingabe, zustand):
-    switcher={
-    "UG":  ,
-    "EG": "Lift im EG",
-    "OG": "Lift im OG"
-    }
-stockwerk = "Lift im UG"
-print(stockwerk)
-while wahl != False:
-    wahl = input('Wählen Sie ein Stockwerk UG, EG oder OG')
-    stockwerk = ZustandWechseln(wahl, stockwerk)
+try:
+    while True:
+        fahrliste = []
+        wohinfahren = input("Bitte geben sie (U)/(E)/(O) für die Stockwerke ein zu denen sie fahren wollen:\n")
+        print("\nEs werden erst alle schon eingegebenen Stockwerke angefahren bevor die nächste Eingabe abgefragt wird.\n")
+        for stockwerke in wohinfahren:
+            if stockwerke in ("U","E","O"):
+                fahrliste.append(stockwerke)
+        if len(fahrliste) > 0:
+            print(fahrliste)
+            for fahrplan in fahrliste:
+                distanz = stockwerkdistanz(fahrplan, woistaufzug, switchlist)
+                if distanz > 0:
+                    hochfahren(distanz)
+                elif distanz < 0:
+                    herunterfahren(abs(distanz))
+                else:
+                    unnecessary += 1
+                woistaufzug = fahrplan
+        else:
+            print("\nSie haben keine Zulässigen Stockwerke eingegeben!\n")
+        print("Der Aufzug hat", unnecessary,"mal unnötig gehalten")
+except Exception as e:
+    print("SYSTEM ERROR ROOT NOT FOUND")
